7986d8e8cf11c16bd9da8b70fc6775c51906948d
YamiSagi <84624853+yamisagi@users.noreply.github.com>
2022-10-08 15:28:37 +0300

    fix: firebase_exceptions

--- lib/services/firebase_exceptions.dart
+++ lib/services/firebase_exceptions.dart
@@ -1,26 +1,22 @@
-// For login.dart 
+// For login.dart
 class UserNotFoundAuthException implements Exception {}
+
 class WrongPasswordAuthException implements Exception {}
 
 class EmailAlreadyExistsAuthException implements Exception {}
 
 // For registering user.
 class UserAlreadyExistsAuthException implements Exception {}
+
 class WeakPasswordAuthException implements Exception {}
+
 class InvalidEmailAuthException implements Exception {}
 
 // Generic exceptions.
 class GenericAuthException implements Exception {}
-class NoInternetConnection implements Exception{}
-class UserNotLoggedInAuthException implements Exception {}
-class NoInitializationException implements Exception {}
 
-// Database exceptions.
-class DatabaseAlreadyInitializedException implements Exception {}
-class UnableToGetDatabaseException implements Exception {}
-class DatabaseNotOpenException implements Exception {}
-class UserAlreadyExist implements Exception{}
-class UserNotFoundException implements Exception{}
-class CouldNotDeleteNoteException implements Exception{}
-class NotesNotFoundException implements Exception{}
-class CouldNotUpdateNoteException implements Exception{}
+class NoInternetConnection implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
+
+class NoInitializationException implements Exception {}
